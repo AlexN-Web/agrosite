8a97fc91f35db3ce2a6f4604138f5f9f930e3da8
AlexN-Web <anayden2@gmail.com>
2025-08-19 22:37:10 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.header .navbar');
+let contactInfo = document.querySelector('.contact-info');
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+};
+
+document.querySelector('#info-btn').onclick = () =>{
+   contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick = () =>{
+   contactInfo.classList.remove('active');
+}
+
+
+
